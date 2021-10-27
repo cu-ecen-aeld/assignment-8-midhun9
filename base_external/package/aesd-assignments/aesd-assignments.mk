c7187d761c2ccee8a04d05a0c7167fb9dbd917e4
midhun9 <midhun.koneru111@gmail.com>
2021-10-27 02:23:09 -0600

    Assignment8_localpass

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '1e00b07fd9ad0516905a7c276fe0a3e115280f14'
+AESD_ASSIGNMENTS_VERSION = '396e3077cafb28165fd241f0161236a389698ca8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ base_external/package/chardevice/ldd.mk
@@ -0,0 +1,21 @@
+#CHARDEVICE
+
+CHARDEVICE_VERSION = 396e3077cafb28165fd241f0161236a389698ca8
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+CHARDEVICE_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-midhun9.git'
+CHARDEVICE_SITE_METHOD = git
+CHARDEVICE_GIT_SUBMODULES = YES
+CHARDEVICE_MODULE_SUBDIRS = aesd-char-driver/
+
+define CHARDEVICE_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
